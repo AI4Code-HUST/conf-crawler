5b8dde3670d09a50f91adda0d6315ae65eccc64c
manhtdd <manh.td120901@gmail.com>
2024-12-10 13:13:32 +0800

    Add logger

--- conf_crawler/main.py
+++ conf_crawler/main.py
@@ -1,5 +1,21 @@
+import logging
+import os
+
+# Configure logging to both a file and the console
+if not os.path.exists("./logs"):
+    os.makedirs("./logs")
+logging.basicConfig(
+    force=True,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(funcName)s:%(lineno)d - %(message)s',
+    handlers=[
+        logging.FileHandler("./logs/connect_to_gmail.log"),  # Log to a file
+        logging.StreamHandler()  # Log to the console
+    ]
+)
+
 def main():
-    print("Hello, world!")
+    logging.info("Hello, world!")
 
 if __name__ == "__main__":
     main()
